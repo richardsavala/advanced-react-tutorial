b0222c05665beb2d92574a2125a54a7532da25f0
Richard <82630326+richardsavala@users.noreply.github.com>
2021-05-15 10:00:40 -0600

    Add files via upload
    fixed if statement in variable handleClick

---
+++ src/tutorial/2-useState-basics.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+// use
+// component name must be uppercase
+// must be in the funstion component body
+// cannot call cositionally
+
+const UseStateBasics = () => {
+  // console.log(useState("hello world"));
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler);
+
+  const [text, setText] = useState("random title");
+
+  const handleClick = () => {
+    if (text === "random title") {
+      setText("hello world");
+    } else {
+      setText("random title");
+    }
+  };
+  return (
+    <>
+      <h1>{text}</h1>
+      <button className="btn" onClick={handleClick}>
+        change title
+      </button>
+    </>
+  );
+};
+
+export default UseStateBasics;
