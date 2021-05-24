c3408770bdde251eb7788855fa7ee1f384e5a38a
Richard <82630326+richardsavala@users.noreply.github.com>
2021-05-23 23:01:26 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from "react";
-import Setup from "./tutorial/1-useState/setup/5-useState-counter";
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 
+const url = "https://course-api.com/react-tours-project";
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
   return (
-    <div className="container">
-      <Setup />
-    </div>
+    <main>
+      <Tours tours={tours} />
+    </main>
   );
 }
 
