e56ecab22a1db40d726b21d5a994f9a930123098
Richard <82630326+richardsavala@users.noreply.github.com>
2021-07-24 08:03:34 -0600

    Add files via upload
    useReducer - useState setup
    in index.js const Index, added const name, const [people, setPeople], const [showModal, setShowModal], and const handleSubmit
    in const Index return, added if statement, form, div, input

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,26 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from "react";
+import Modal from "./Modal";
+import { data } from "../../../data";
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const name = useState("");
+  const [people, setPeople] = useState(data);
+  const [showModal, setShowModal] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      {showModal && <Modal />}
+      <form onSubmit={handleSubmit}>
+        <div className="">
+          <input type="text" value={name} onChange={(e) => e.target.value} />
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default Index;
