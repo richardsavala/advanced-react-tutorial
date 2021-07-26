6229a3d9194d0013833f43430c3d3cfc551d3b35
Richard <82630326+richardsavala@users.noreply.github.com>
2021-07-25 20:59:44 -0600

    Add files via upload
    useReducer - useState setup
    in index.js corrected error
    in the returned form, placed e.target.value within setName()

---
+++ src/tutorial/6-useReducer/index.js
@@ -0,0 +1,36 @@
+import React, { useState, useReducer } from "react";
+import Modal from "./Modal";
+import { data } from "../../../data";
+// reducer function
+
+const Index = () => {
+  const [name, setName] = useState("");
+  const [people, setPeople] = useState(data);
+  const [showModal, setShowModal] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name) {
+      showModal(true);
+    } else {
+      showModal(true);
+    }
+  };
+
+  return (
+    <>
+      {showModal && <Modal />}
+      <form onSubmit={handleSubmit} className="form">
+        <div className="">
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <button type="submit">add</button>
+      </form>
+    </>
+  );
+};
+
+export default Index;
