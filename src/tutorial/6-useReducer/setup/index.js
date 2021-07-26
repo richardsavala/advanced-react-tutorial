6f88ac4b602a0c67b905ad735250088b5c9c47f1
Richard <82630326+richardsavala@users.noreply.github.com>
2021-07-25 23:51:29 -0600

    Add files via upload
    useReducer - useState setup
    in index.js return, added if statement to iterate over people with map and use div key={person.id} to return person.name

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -4,15 +4,17 @@ import { data } from "../../../data";
 // reducer function
 
 const Index = () => {
-  const name = useState("");
+  const [name, setName] = useState("");
   const [people, setPeople] = useState(data);
   const [showModal, setShowModal] = useState(false);
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
-      showModal(true);
+      setShowModal(true);
+      setPeople([...people, { id: new Date().getTime().toString(), name }]);
+      setName("");
     } else {
-      showModal(true);
+      setShowModal(true);
     }
   };
 
@@ -21,10 +23,21 @@ const Index = () => {
       {showModal && <Modal />}
       <form onSubmit={handleSubmit} className="form">
         <div className="">
-          <input type="text" value={name} onChange={(e) => e.target.value} />
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
         <button type="submit">add</button>
       </form>
+      {people.map((person) => {
+        return (
+          <div key={person.id}>
+            <h4>{person.name}</h4>
+          </div>
+        );
+      })}
     </>
   );
 };
