a8533a39806062655257a770536d2e04a3adb169
Richard <82630326+richardsavala@users.noreply.github.com>
2021-07-31 23:45:19 -0600

    Add files via upload
    useReducer - remove item
    in index.js replaced const reducer with import reducer from reducer.js
    added reducer.js with export const reducer

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -2,36 +2,8 @@ import React, { useState, useReducer } from "react";
 import Modal from "./Modal";
 import { data } from "../../../data";
 // reducer function
+import { reducer } from "./reducer";
 
-const reducer = (state, action) => {
-  console.log(state);
-  if (action.type === "ADD_ITEM") {
-    const newPeople = [...state.people, action.payload];
-    return {
-      ...state,
-      people: newPeople,
-      isModalOpen: true,
-      modalContent: "item added",
-    };
-  }
-  if (action.type === "NO_VALUE") {
-    return {
-      ...state,
-      isModalOpen: true,
-      modalContent: "please enter value",
-    };
-  }
-  if (action.type === "CLOSE_MODAL") {
-    return { ...state, isModalOpen: false };
-  }
-  if (action.type === "REMOVE_ITEM") {
-    const newPeople = state.people.filter(
-      (person) => person.id !== action.payload
-    );
-    return { ...state, people: newPeople };
-  }
-  throw new Error("no matching action type");
-};
 const defaultState = {
   people: [],
   isModalOpen: false,
